d59aa1a8a5f51994cc3b8ee968589ead47180ee5
JIHUN <mbh2857@naver.com>
2024-03-29 01:34:55 +0000

    bash

--- train.sh
+++ train.sh
@@ -2,5 +2,4 @@ python train.py \
     --content_path images/fashion4.png \
     --save_dir "model_output" \
     --content_name "Jeans" \
-    --text "black leather" \
-    --max_step 600
+    --text "black leather" 
